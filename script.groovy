070b228cce49fafcf154f274f0065e468d88f1e2
Saurabh <Saurabhkr952@gmail.com>
2022-11-17 19:57:56 +0530

    Groovy Script

---
+++ script.groovy
@@ -0,0 +1,20 @@
+def incrementVersion(){
+    echo 'Increment version'
+    def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+    def version = matcher[0][1]
+    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
+}
+def buildImage() {
+    echo "building the docker image..."
+    sh "docker build -t saurabhkr952/java-maven-app:$IMAGE_NAME ."
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push saurabhkr952/my-portfolio:$IMAGE_NAME"
+    }
+} 
+
+return this
