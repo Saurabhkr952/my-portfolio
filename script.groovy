6dd62fa73eedc312e38e76ec13b568ce85716031
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2022-11-19 10:49:23 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -25,7 +25,7 @@ def k8sManifest() {
 
 def update_k8s_manifest() {
     echo "pushing updated manifest to repository"
-    withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
+    withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
     sh "git config --global user.email 'saurabhkr952@gmail.com'"
     sh "git config --global user.name 'Saurabhkr952'"
     sh "cat my-portfolio.yaml"
