ad2b0d8b299d9b3702c6773da53b78aaeb39fa59
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2022-11-20 09:25:45 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -19,17 +19,20 @@ def deployApp() {
 
 
 def k8sManifest() {
-    sh "git clone https://github.com/Saurabhkr952/k8s_manifest"
-    sh "cd k8s_manifest"
-    sh "sed -i 's+saurabhkr952/my-portfolio:.*+saurabhkr952/my-portfolio:${IMAGE_NAME}-$BUILD_NUMBER+g' /var/jenkins_home/workspace/my-portfolio-build/k8s_manifest/my-portfolio.yaml"
+    git credentialsId: 'github-credentials', 
+    url: 'https://github.com/Saurabhkr952/k8s_manifest.git',
+    branch: 'main'
+    sh "sed -i 's+saurabhkr952/my-portfolio:.*+saurabhkr952/my-portfolio:${IMAGE_NAME}-$BUILD_NUMBER+g' my-portfolio.yaml"
+}
+    
 }
 def update_k8s_manifest() {
     echo "pushing updated manifest to repository"
     withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
-    sh "/var/jenkins_home/workspace/my-portfolio-build/k8s_manifest/git add my-portfolio.yaml"
-    sh "/var/jenkins_home/workspace/my-portfolio-build/k8s_manifest/git commit -m 'Updated the my-portfolio yaml | Image Version=$IMAGE_NAME'"
-    sh "/var/jenkins_home/workspace/my-portfolio-build/k8s_manifest/git remote -v"
-    sh "/var/jenkins_home/workspace/my-portfolio-build/k8s_manifest/git push https://$password@github.com/Saurabhkr952/k8s_manifest.git HEAD:main" 
+    sh "git add my-portfolio.yaml"
+    sh "git commit -m 'Updated the my-portfolio yaml | Image Version=$IMAGE_NAME'"
+    sh "git remote -v"
+    sh "git push https://$password@github.com/Saurabhkr952/k8s_manifest.git HEAD:main" 
     }      
 }
 
