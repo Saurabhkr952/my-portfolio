8229032077760f3255e9b13a04b5658f6ecd563a
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2022-11-22 11:19:34 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -8,14 +8,14 @@ def incrementVersion(){
 
 def buildImage() {
     echo 'Building Docker Image'
-    sh "docker build -t saurabhkr952/my-portfolio:$IMAGE_NAME-$BUILD_NUMBER ."
+    sh "docker build -t saurabhkr952/my-portfolio:$IMAGE_NAME ."
 } 
 
 def deployApp() {
     echo 'deploying the application...'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push saurabhkr952/my-portfolio:$IMAGE_NAME-$BUILD_NUMBER"
+        sh "docker push saurabhkr952/my-portfolio:$IMAGE_NAME-"
     }
 }
 
@@ -29,7 +29,7 @@ def k8sManifest() {
 def update_k8s_manifest() {
     echo "pushing updated manifest to repository"
     withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
-        sh "sed -i 's+saurabhkr952/my-portfolio:.*+saurabhkr952/my-portfolio:${env.IMAGE_NAME}+g' my-portfolio.yaml"
+    sh "sed -i 's+saurabhkr952/my-portfolio:.*+saurabhkr952/my-portfolio:${env.IMAGE_NAME}+g' my-portfolio.yaml"
     sh "git add my-portfolio.yaml"
     sh "git commit -m 'Updated the my-portfolio yaml | Image Version=$IMAGE_NAME'"
     sh "git remote -v"
