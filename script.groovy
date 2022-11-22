52fcf98c6e2a5551a3806b56202640544f088fc3
Saurabh Kumar <32189783+Saurabhkr952@users.noreply.github.com>
2022-11-22 10:17:19 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,9 +3,14 @@ def incrementVersion(){
     def matcher = readFile('package.json') =~ '"version": (.+),'
     def version = matcher[0][1]
     env.IMAGE_NAME = "${version}"
+    env.TEXT_MANIPULATE = "${textManipulate}"
 }
+def textManipulate() {
+    sh "sed -i 's+saurabhkr952/my-portfolio:.*+saurabhkr952/my-portfolio:$IMAGE_NAME-$BUILD_NUMBER+g' my-portfolio.yaml."
+} 
+
 def buildImage() {
-    echo "Building the docker image..."
+    echo 'Building Docker Image'
     sh "docker build -t saurabhkr952/my-portfolio:$IMAGE_NAME-$BUILD_NUMBER ."
 } 
 
@@ -27,7 +32,7 @@ def k8sManifest() {
 def update_k8s_manifest() {
     echo "pushing updated manifest to repository"
     withCredentials([usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
-    sh "sed -i 's+saurabhkr952/my-portfolio:.*+saurabhkr952/my-portfolio:$IMAGE_NAME-$BUILD_NUMBER+g' my-portfolio.yaml"
+    $TEXT_MANIPULATE
     sh "git add my-portfolio.yaml"
     sh "git commit -m 'Updated the my-portfolio yaml | Image Version=$IMAGE_NAME'"
     sh "git remote -v"
